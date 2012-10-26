a465d896790ae428578aeac4e5fd51448fdd9001
Michael Burtscher <mike@mbnet.at>
2012-10-26 17:47:00 +0200

    fixed date issues with datepicker and improved multiday event selection

--- js/admin_new.js
+++ js/admin_new.js
@@ -11,19 +11,7 @@ jQuery(document).ready(function( $ ) {
 		$("#multiday").attr('checked', true);
 	}
 	
-	// Toogle end_date view
-	$("#multiday").click(function() {
-		if (this.checked) {
-			$("#end_date").val($("#start_date").val());
-			$("#end_date_area").fadeIn();
-		}
-		else {
-			$("#end_date_area").fadeOut();
-			$("#end_date").val($("#start_date").val());
-		}
-	});
-	
-	// Datepicker
+	// Datepickers
 	$("#start_date").datepicker( {
 		dateFormat: "yy-mm-dd",
 		firstDay: 1,
@@ -32,7 +20,10 @@ jQuery(document).ready(function( $ ) {
 		numberOfMonths: 3,
 		constrainInput: true,
 		onClose: function(selectedDate) {
-			$("#end_date").datepicker("option", "minDate", selectedDate);
+			minDate = new Date(selectedDate);
+			timestamp = minDate.getTime() + 1*24*60*60*1000;
+			minDate.setTime(timestamp);
+			$("#end_date").datepicker("option", "minDate", minDate);
 		}
 	});
 	$("#end_date").datepicker( {
@@ -41,10 +32,21 @@ jQuery(document).ready(function( $ ) {
 		changeMonth: true,
 		changeYear: true,
 		numberOfMonths: 3,
-		constrainInput: true,
-		minDate: $("#start_date").val(),
-		onClose: function(selectedDate) {
-			$("#start_date").datepicker("option", "maxDate", selectedDate);
+		constrainInput: true
+	});
+	
+	// Toogle end_date visibility and insert the correct date
+	$("#multiday").click(function() {
+		var enddate = $("#start_date").datepicker("getDate");
+		if (this.checked) {
+			timestamp = enddate.getTime() + 1*24*60*60*1000;
+			enddate.setTime(timestamp);
+			$("#end_date_area").fadeIn();
 		}
+		else {
+			$("#end_date_area").fadeOut();
+			$("#end_date").datepicker("option", "minDate", null);
+		}
+		$("#end_date").datepicker("setDate", enddate);
 	});
 });
