8a5c2c772a69b357360cd957877ee8040473d23d
Michael Burtscher <mike@mbnet.at>
2013-03-16 09:06:14 +0100

    removed link to not available settings page

--- php/admin.php
+++ php/admin.php
@@ -184,9 +184,9 @@ class el_admin {
 					Additonally you have to insert the correct Shortcode ID on the linked page. This ID describes which shortcode should be used on the given page or post if you have more than one.
 					So the standard value "1" is normally o.k., but you can check the ID if you have a look into the URL of an event link on your linked page or post.
 					The ID is given behind the "_" (e.g. <i>http://www.your-homepage.com/?page_id=99&event_id_<strong>1</strong>=11</i>).
-				</p>
-				<p>Be sure to also check the <a href="admin.php?page=el_admin_settings">settings page</a> to get Event List behaving just the way you want.</p>
-			</div>';
+				</p>';
+				//<p>Be sure to also check the <a href="admin.php?page=el_admin_settings">settings page</a> to get Event List behaving just the way you want.</p>
+		$out .= '</div>';
 		$out .= $this->html_atts();
 		echo $out;
 	}
