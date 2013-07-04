fc5053cd6791aae81c5d7dd852cea48d07e29b79
Michael Burtscher <mike@mbnet.at>
2013-07-04 10:10:51 +0200

    whitespace fixes

--- admin/includes/admin-about.php
+++ admin/includes/admin-about.php
@@ -9,7 +9,7 @@ class EL_Admin_About {
 
 	public static function &get_instance() {
 		// Create class instance if required
-		if( !isset( self::$instance ) ) {
+		if(!isset(self::$instance)) {
 			self::$instance = new EL_Admin_About();
 		}
 		// Return class instance
