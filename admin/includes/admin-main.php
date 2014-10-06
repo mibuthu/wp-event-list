f0ebc3393f688986372de787698965ac6c96068a
Michael Burtscher <mike@mbnet.at>
2014-10-06 11:06:40 +0200

    added "Add new" and "Duplicate" button in edit event view

--- admin/includes/admin-main.php
+++ admin/includes/admin-main.php
@@ -101,14 +101,15 @@ class EL_Admin_Main {
 
 	private function show_page_header($action, $editview=false) {
 		if($editview) {
-			$header = 'Edit Event';
+			$duplicate_link = add_query_arg(array('id'=>$_GET['id'], 'action'=>'copy'), '?page=el_admin_new');
+			$header = 'Edit Event <a href="'.$duplicate_link.'" class="add-new-h2">'.__('Duplicate').'</a>';
 		}
 		else {
-			$header = 'Events <a href="?page=el_admin_new" class="add-new-h2">Add New</a>';
+			$header = 'Events';
 		}
 		echo '
 			<div class="wrap">
-				<div id="icon-edit-pages" class="icon32"><br /></div><h2>'.$header.'</h2>';
+				<div id="icon-edit-pages" class="icon32"><br /></div><h2>'.$header.' <a href="?page=el_admin_new" class="add-new-h2">'.__('Add New').'</a></h2>';
 		$this->show_message($action);
 	}
 
