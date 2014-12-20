3009f4807923f221a64f0872cab4bd72bd403a71
Michael Burtscher <mike@mbnet.at>
2014-12-21 00:10:23 +0100

    added some missing translation function and some german translations
    issue #24

--- admin/includes/admin-categories.php
+++ admin/includes/admin-categories.php
@@ -89,7 +89,7 @@ class EL_Admin_Categories {
 					</div>';
 				}
 				else {
-					$out .= '<div id="message" class="error below-h2"><p><strong>Error while deleting category "'.$_GET['slug'].'".</strong></p></div>';
+					$out .= '<div id="message" class="error below-h2"><p><strong>'.sprintf(__('Error while deleting category "%s"','eventlist'), $_GET['slug']).'.</strong></p></div>';
 				}
 			}
 		}
@@ -116,19 +116,19 @@ class EL_Admin_Categories {
 				if(!isset($_POST['id'])) {
 					// add new category
 					if($this->categories->add_category($_POST)) {
-						$out .= '<div id="message" class="updated below-h2"><p><strong>New Category "'.$_POST['name'].'" was added.</strong></p></div>';
+						$out .= '<div id="message" class="updated below-h2"><p><strong>'.sprintf(__('New Category "%s" was added','eventlist'), $_POST['name']).'.</strong></p></div>';
 					}
 					else {
-						$out .= '<div id="message" class="error below-h2"><p><strong>Error: New Category "'.$_POST['name'].'" could not be added.</strong></p></div>';
+						$out .= '<div id="message" class="error below-h2"><p><strong>'.sprintf(__('Error: New Category "$s" could not be added','eventlist'), $_POST['name']).'.</strong></p></div>';
 					}
 				}
 				else {
 					// edit category
 					if($this->categories->edit_category($_POST, $_POST['id'])) {
-						$out .= '<div id="message" class="updated below-h2"><p><strong>Category "'.$_POST['id'].'" was modified.</strong></p></div>';
+						$out .= '<div id="message" class="updated below-h2"><p><strong>'.sprintf(__('Category "%s" was modified','eventlist'), $_POST['id']).'.</strong></p></div>';
 					}
 					else {
-						$out .= '<div id="message" class="error below-h2"><p><strong>Error: Category "'.$_POST['id'].'" could not be modified.</strong></p></div>';
+						$out .= '<div id="message" class="error below-h2"><p><strong>'.sprintf(__('Error: Category "%s" could not be modified','eventlist'), $_POST['id']).'.</strong></p></div>';
 					}
 				}
 			}
@@ -137,7 +137,7 @@ class EL_Admin_Categories {
 		if($is_disabled) {
 			$out .= '<div id="message" class="updated"><p>'.__('Categories are automatically synced with the post categories.<br />
 			                                                    Because of this all options to add new categories or editing existing categories are disabled.<br />
-			                                                    If you want to manually edit the categories you have to disable this option.').'</p></div>';
+			                                                    If you want to manually edit the categories you have to disable this option.','eventlist').'</p></div>';
 		}
 		return $out;
 	}
@@ -162,17 +162,17 @@ class EL_Admin_Categories {
 		}
 		// Category Name
 		$out .= '
-				<div class="form-field form-required"><label for="name">Name: </label>';
+				<div class="form-field form-required"><label for="name">'.__('Name','eventlist').': </label>';
 		$out .= $this->functions->show_text('name', $cat_data['name'], $is_disabled);
 		$out .= '<p>'.__('The name is how it appears on your site.','eventlist').'</p></div>';
 		// Category Slug
 		$out .= '
-				<div class="form-field"><label for="name">Slug: </label>';
+				<div class="form-field"><label for="name">'.__('Slug','eventlist').': </label>';
 		$out .= $this->functions->show_text('slug', $cat_data['slug'], $is_disabled);
 		$out .= '<p>'.__('The “slug” is the URL-friendly version of the name. It is usually all lowercase and contains only letters, numbers, and hyphens.','eventlist').'</p></div>';
 		// Category Parent
 		$out .= '
-				<div class="form-field"><label for="parent">Parent: </label>';
+				<div class="form-field"><label for="parent">'.__('Parent','eventlist').': </label>';
 		$cat_array = $this->categories->get_cat_array('name', 'asc');
 		$option_array = array('' => __('None','eventlist'));
 		$class_array = array();
@@ -187,7 +187,7 @@ class EL_Admin_Categories {
 		$out .= '<p>'.__('Categories can have a hierarchy. You might have a Jazz category, and under that have children categories for Bebop and Big Band. Totally optional.','eventlist').'</p></div>';
 		// Category Description
 		$out .= '
-				<div class="form-field"><label for="name">Description: </label>';
+				<div class="form-field"><label for="name">'.__('Description','eventlist').': </label>';
 		$out .= $this->functions->show_textarea('desc', $cat_data['desc'], $is_disabled);
 		$out .= '</div>
 				<p class="submit"><input type="submit" class="button-primary" value="'.$button_text.'" id="submitbutton"'.$this->functions->get_disabled_text($is_disabled).'></p>';
--- admin/includes/category_table.php
+++ admin/includes/category_table.php
@@ -24,8 +24,8 @@ class EL_Category_Table extends WP_List_Table {
 		$this->is_disabled = $is_disabled;
 		//Set parent defaults
 		parent::__construct( array(
-			'singular'  => 'event',     //singular name of the listed records
-			'plural'    => 'events',    //plural name of the listed records
+			'singular'  => __('event','eventlist'),     //singular name of the listed records
+			'plural'    => __('events','eventlist'),    //plural name of the listed records
 			'ajax'      => false        //does this table support ajax?
 		) );
 	}
@@ -67,8 +67,8 @@ class EL_Category_Table extends WP_List_Table {
 		if(!$this->is_disabled) {
 			// prepare Actions
 			$actions = array(
-				'edit'      => '<a href="?page='.$_REQUEST['page'].'&amp;id='.$item['slug'].'&amp;action=edit">Edit</a>',
-				'delete'    => '<a href="#" onClick="eventlist_deleteCategory(\''.$item['slug'].'\');return false;">Delete</a>'
+				'edit'      => '<a href="?page='.$_REQUEST['page'].'&amp;id='.$item['slug'].'&amp;action=edit">'.__('Edit','eventlist').'</a>',
+				'delete'    => '<a href="#" onClick="eventlist_deleteCategory(\''.$item['slug'].'\');return false;">'.__('Delete','eventlist').'</a>'
 			);
 			//Return the title contents
 			$out .= $this->row_actions($actions);
@@ -104,10 +104,10 @@ class EL_Category_Table extends WP_List_Table {
 	public function get_columns() {
 		return array(
 			'cb'         => $this->is_disabled ? '' : '<input type="checkbox" />', //Render a checkbox instead of text
-			'name'       => 'Name',
-			'desc'       => 'Description',
-			'slug'       => 'Slug',
-			'num_events' => 'Events'
+			'name'       => __('Name','eventlist'),
+			'desc'       => __('Description','eventlist'),
+			'slug'       => __('Slug','eventlist'),
+			'num_events' => __('Events','eventlist')
 		);
 	}
 
@@ -142,7 +142,7 @@ class EL_Category_Table extends WP_List_Table {
 	public function get_bulk_actions() {
 		if(!$this->is_disabled) {
 			$actions = array(
-				'delete_bulk' => 'Delete'
+				'delete_bulk' => __('Delete','eventlist')
 			);
 			return $actions;
 		}
