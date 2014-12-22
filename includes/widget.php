7f6310db49e63d7ba810b8980176a8253489665e
Michael Burtscher <mike@mbnet.at>
2014-12-22 13:49:04 +0100

    Moved widget items helptexts to separate file and only include them in the admin page

--- includes/widget.php
+++ includes/widget.php
@@ -17,123 +17,35 @@ class EL_Widget extends WP_Widget {
 		parent::__construct(
 				'event_list_widget', // Base ID
 				'Event List', // Name
-				array('description' => __('This widget displays a list of upcoming events. If you want to enable a link to the events or to the event page you have to insert a link to the event-list page or post.','eventlist')) // Args
+				array('description' => __('With this widget a list of upcoming events can be displayed.','eventlist')) // Args
 		);
 
 		// define all available items
 		$this->items = array(
-			'title' =>                array('type'          => 'text',
-			                                'std_value'     => __('Upcoming events','eventlist').':',
-			                                'caption'       => __('Title','eventlist'),
-			                                'caption_after' => null,
-			                                'tooltip'       => __('This option defines the displayed title for the widget.','eventlist'),
-			                                'form_style'    => null,
-			                                'form_width'    => null),
-
-			'cat_filter' =>           array('type'          => 'text',
-			                                'std_value'     => 'all',
-			                                'caption'       => __('Category Filter','eventlist').':',
-			                                'caption_after' => null,
-			                                'tooltip'       => __('This option defines the categories of which events are shown. The standard is all or an empty string to show all events. Specify a category slug or a list of category slugs to only show events of the specified categories. See description of the shortcode attribute cat_filter for detailed info about all possibilities.','eventlist'),
-			                                'form_style'    => 'margin:0 0 0.8em 0',
-			                                'form_width'    => null),
-
-			'num_events' =>           array('type'          => 'text',
-			                                'std_value'     => '3',
-			                                'caption'       => __('Number of listed events','eventlist').':',
-			                                'caption_after' => null,
-			                                'tooltip'       => __('The number of upcoming events to display','eventlist'),
-			                                'form_style'    => '',
-			                                'form_width'    => 30),
-
-			'title_length' =>         array('type'          => 'text',
-			                                'std_value'     => '0',
-			                                'caption'       => __('Truncate event title to','eventlist'),
-			                                'caption_after' => __('characters','eventlist'),
-			                                'tooltip'       => __('This option defines the number of displayed characters for the event title. Set this value to 0 to view the full title.','eventlist'),
-			                                'form_style'    => null,
-			                                'form_width'    => 30),
-
-			'show_starttime' =>       array('type'          => 'checkbox',
-			                                'std_value'     => 'true',
-			                                'caption'       => __('Show event starttime','eventlist'),
-			                                'caption_after' => null,
-			                                'tooltip'       => __('This option defines if the event start time will be displayed.','eventlist'),
-			                                'form_style'    => null,
-			                                'form_width'    => null),
-
-			'show_location' =>        array('type'          => 'checkbox',
-			                                'std_value'     => 'false',
-			                                'caption'       => __('Show event location','eventlist'),
-			                                'caption_after' => null,
-			                                'tooltip'       => __('This option defines if the event location will be displayed.','eventlist'),
-			                                'form_style'    => 'margin:0 0 0.2em 0',
-			                                'form_width'    => null),
-
-			'location_length' =>      array('type'          => 'text',
-			                                'std_value'     => '0',
-			                                'caption'       => __('Truncate location to','eventlist'),
-			                                'caption_after' => __('characters','eventlist'),
-			                                'tooltip'       => __('If the event location is diplayed this option defines the number of displayed characters. Set this value to 0 to view the full location.','eventlist'),
-			                                'form_style'    => 'margin:0 0 0.6em 0.9em',
-			                                'form_width'    => 30),
-
-			'show_details' =>         array('type'          => 'checkbox',
-			                                'std_value'     => 'false',
-			                                'caption'       => __('Show event details','eventlist'),
-			                                'caption_after' => null,
-			                                'tooltip'       => __('This option defines if the event details will be displayed.','eventlist'),
-			                                'form_style'    => 'margin:0 0 0.2em 0',
-			                                'form_width'    => null),
-
-			'details_length' =>       array('type'          => 'text',
-			                                'std_value'     => '0',
-			                                'caption'       => __('Truncate details to','eventlist'),
-			                                'caption_after' => __('characters','eventlist'),
-			                                'tooltip'       => __('If the event details are diplayed this option defines the number of diplayed characters. Set this value to 0 to view the full details.','eventlist'),
-			                                'form_style'    => 'margin:0 0 0.6em 0.9em',
-			                                'form_width'    => 30),
-
-			'url_to_page' =>          array('type'          => 'text',
-			                                'std_value'     => '',
-			                                'caption'       => __('URL to the linked Event List page','eventlist').':',
-			                                'caption_after' => null,
-			                                'tooltip'       => __('This option defines the url to the linked Event List page. This option is required if you want to use one of the options below.','eventlist'),
-			                                'form_style'    => 'margin:0 0 0.4em 0',
-			                                'form_width'    => null),
-
-			'sc_id_for_url' =>        array('type'          => 'text',
-			                                'std_value'     => '1',
-			                                'caption'       => __('Shortcode ID on linked page','eventlist').':',
-			                                'caption_after' => null,
-			                                'tooltip'       => __('This option defines the shortcode-id for the Event List on the linked page. Normally the standard value 1 is correct, you only have to change it if you use multiple event-list shortcodes on the linked page.','eventlist'),
-			                                'form_style'    => null,
-			                                'form_width'    => 30),
-
-			'link_to_event' =>        array('type'          => 'checkbox',
-			                                'std_value'     => 'false',
-			                                'caption'       => __('Add links to the single events','eventlist'),
-			                                'caption_after' => null,
-			                                'tooltip'       => __('With this option you can add a link to the single event page for every displayed event. You have to specify the url to the page and the shortcode id option if you want to use it.','eventlist'),
-			                                'form_style'    => 'margin-left:0.8em',
-			                                'form_width'    => null),
-
-			'link_to_page' =>         array('type'          => 'checkbox',
-			                                'std_value'     => 'false',
-			                                'caption'       => __('Add a link to the Event List page','eventlist'),
-			                                'caption_after' => null,
-			                                'tooltip'       => __('With this option you can add a link to the event-list page below the diplayed events. You have to specify the url to page option if you want to use it.','eventlist'),
-			                                'form_style'    => 'margin:0 0 0.2em 0.8em',
-			                                'form_width'    => null),
-
-			'link_to_page_caption' => array('type'          => 'text',
-			                                'std_value'     => __('show events page','eventlist'),
-			                                'caption'       => __('Caption for the link','eventlist').':',
-			                                'caption_after' => null,
-			                                'tooltip'       => __('This option defines the text for the link to the Event List page if the approriate option is selected.','eventlist'),
-			                                'form_style'    => 'margin:0 0 1em 2.5em',
-			                                'form_width'    => null),
+			'title'                => array('std_value' => __('Upcoming events','eventlist').':'),
+			'cat_filter'           => array('std_value' => 'all'),
+			'num_events'           => array('std_value' => '3'),
+			'title_length'         => array('std_value' => '0'),
+			'show_starttime'       => array('std_value' => 'true'),
+			'show_location'        => array('std_value' => 'false'),
+			'location_length'      => array('std_value' => '0'),
+			'show_details'         => array('std_value' => 'false'),
+			'details_length'       => array('std_value' => '0'),
+			'url_to_page'          => array('std_value' => ''),
+			'sc_id_for_url'        => array('std_value' => '1'),
+			'link_to_event'        => array('std_value' => 'false'),
+			'link_to_page'         => array('std_value' => 'false'),
+			'link_to_page_caption' => array('std_value' => __('show events page','eventlist')),
 		);
+
+		add_action('admin_init', array(&$this, 'load_widget_items_helptexts'), 2);
+	}
+
+	public function load_widget_items_helptexts() {
+		require_once(EL_PATH.'includes/widget_helptexts.php');
+		foreach($widget_items_helptexts as $name => $values) {
+			$this->items[$name] = array_merge($this->items[$name], $values);
+		}
 	}
 
 	/**
