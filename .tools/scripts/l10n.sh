0af50a1933712085d1dec81bafb16837a05b4d65
Michael Burtscher <mike@mbnet.at>
2015-07-18 21:58:44 +0200

    improved and extended l10n script

--- .tools/scripts/l10n.sh
+++ .tools/scripts/l10n.sh
@@ -10,6 +10,7 @@ plugin_slug="$(basename $(ls "${plugin_path}"/*.php) .php)"
 lang_path="${plugin_path}/${lang_dir}"
 lang_source="${lang_path}/${plugin_slug}.pot"
 debug=0
+tx_arg=""
 
 # get project information from the plugin header
 plugin_name="$(awk -F: '/Plugin Name:/ { print $2 }' "${plugin_path}/${plugin_slug}.php" | sed 's/^ *//g')"
@@ -27,13 +28,11 @@ declare -A commands
 commands=(
 	[update_source]="l10n_update_source|creates or updates the language source file (*.pot)"
 	[push_source]="l10n_push_source|push local changes of source pot file to the transifex server"
-	[pull_translations]="l10n_pull_translations|pull all translation files from the transifex server"
+	[pull_translations]="l10n_pull_translations|pull all translation files from the transifex server, set -a cmd_option to download all translations"
+	[pull_translation]="l10n_pull_translation|pull a single translation file, the language (e.g. de_DE) must be given as a cmd_option"
 	[compile_translations]="l10n_compile_translations|compile all po translation files to mo files"
 	[status]="l10n_status|shows the status of the transifex repository"
 	[help]="l10n_help|show this help message and exit"
-#	[tx_init]="|not implemented yet"
-#	[tx_set]="|not implemented yet"
-#	[add_language]="|not implemented yet"
 )
 
 # ===== FUNCTIONS ===== #
@@ -41,19 +40,19 @@ commands=(
 # Function to print help messages
 # parameters: $1 ... exit code (optional)   The script will exit with the given exit code (default=0).
 function l10n_help() {
-	echo "Usage: $(basename "$0") [option] command [cmd_options]"
+	echo "Usage: $(basename "$0") [option] command [cmd_option]"
 	echo ""
 	echo "This script handles all required task for multi localisation support in Wordpress"
 	echo "plugins and the exchange the language files with Transifex service."
 	echo ""
 	echo "Options:"
 	for option in "${!options[@]}"; do
-		printf "  %-18s%s\n" "$option" "${options[$option]#*|}"
+		printf "  %-22s%s\n" "$option" "${options[$option]#*|}"
 	done
 	echo ""
 	echo "Commands:"
 	for command in "${!commands[@]}"; do
-		printf "  %-18s%s\n" "$command" "${commands[$command]#*|}"
+		printf "  %-22s%s\n" "$command" "${commands[$command]#*|}"
 	done
 	if [[ $1 =~ ^[0-9]+$ ]] ; then
 		echo -e "\nScript aborted! You can try to enable debug messages with -d if you don't know why."
@@ -67,6 +66,7 @@ function l10n_help() {
 # parameters: none
 function l10n_enable_debug() {
 	debug=1
+	tx_arg="-d"
 	# print some general debug messages
 	echo "Plugin Slug: $plugin_slug"
 	echo "Language Path :$lang_path"
@@ -95,13 +95,32 @@ function l10n_update_source() {
 # Function to push the source pot file to the Transifex server
 # parameters: none
 function l10n_push_source() {
-	tx push -s
+	tx ${tx_arg} push -s
 }
 
 # Function to pull the translation files from the Transifex server
-# parameters: none
+# parameters: $1 ... cmd_option (optional)    With the value "-a" all translation files will be downloads (standard: only locally available translations)
 function l10n_pull_translations() {
-	tx pull
+	local arg=""
+	[ "$1" = "-a" ] && arg="-a" 
+	tx ${tx_arg} pull ${arg}
+}
+
+# Function to pull a translation file from the Transifex server (normally used to add a new translation which isn't availabe locally)
+# parameters: $1 ... cmd_option (required)    The language to download must be provided (e.g. "de_DE")
+function l10n_pull_translation() {
+	if [ -z "$1" ]; then
+		# show error, print help, then exit (if no cmd_option was provided)
+		echo -e "ERROR: Required cmd_option (language to download) is missing!\n"
+		l10n_help 1
+	fi
+	if [[ "$1" =~ [a-z]{2}_[A-Z]{2} ]]; then
+		tx ${tx_arg} pull -l $1
+	else
+		# show error, print help, then exit (if cmd_option in wrong format was provided)
+		echo -e "ERROR: Required cmd_option (language to download) is given in a wrong format!\n"
+		l10n_help 1
+	fi
 }
 
 # Function to compile all po translation files to mo files
@@ -117,12 +136,13 @@ function l10n_compile_translations() {
 # Function to show the status of the Transifex repository (tx status)
 # parameters: none
 function l10n_status() {
-	tx status
+	tx ${tx_arg} status
 }
 
 # ===== MAIN PROGRAM ===== #
 
 arg="$1"
+cmd_option="$2"
 
 # check for option args (only 1 option can be handled)
 if [ "${arg:0:1}" = "-" ]; then
@@ -131,6 +151,7 @@ if [ "${arg:0:1}" = "-" ]; then
 		if [ "${optionname%, *}" = "$arg" -o "${optionname#*, }" = "$arg" ]; then
 			valid_option=1
 			arg=$2
+			cmd_option=$3
 			${options[$optionname]%%|*}
 			break
 		fi
@@ -149,7 +170,7 @@ if [ -z "$arg" ]; then
 	l10n_help 1
 fi
 if [ -n "${commands[$arg]}" ]; then
-	${commands[$arg]%%|*}
+	${commands[$arg]%%|*} ${cmd_option}
 else
 	# show error, print help, then exit (if an invalid command was provided)
 	echo -e "ERROR: Invalid command provided!\n"
