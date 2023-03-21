09a95470e106dcdcd6d9c5d1aec9d6b0dff600fc
Whatareyoulookin4 <121126488+Whatareyoulookin4@users.noreply.github.com>
2023-03-21 04:15:26 -0500

    ok

--- src/admin/js/admin_main.js
+++
@@ -1,6 +0,0 @@
-// Javascript functions for event-list admin_main page
-
-jQuery(document).ready(function($) {
-	// Add import button to page title actions
-	$("a.page-title-action").first().after('<a href="edit.php?post_type=el_events&action=import" class="add-new-h2">Import</a>');
-});
